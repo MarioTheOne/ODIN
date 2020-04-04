cb1000f18cf3424d6b17943bc3ee390881acc715
MarioTheOne <m.a.prado89@outlook.com>
2020-04-03 20:45:33 -0400

    Starting Migration to NetworkX 2
    -All scripts compatible with NetworkX 1.x were moved to the folder odin_nx1
    -Readme updated

--- ODIN_main.py
+++ ODIN_main.py
@@ -1,11 +1,13 @@
-import networkx as nx
-import ODIN_twitter_network_manager as tw_manager
-import ODIN_network_manager as net_manager
-import ODIN_visualization as vis
-import ODIN_graph_clustering as GClustering
-import ODIN_anomaly_detection as AnD
-import operator
-
+from odin_nx1 import ODIN_network_manager as net_manager, ODIN_twitter_network_manager as tw_manager
 
 print('Welcome to project ODIN! this project is currently under development. It will become more stable with time ;)')
+
+#us_elections_2018_retweets = \
+#    net_manager.read_network_from_gpickle_file('Networks/us_elections_2018_retweets/us_elections_retweets.gpickle')
+
+
+DiG = tw_manager.read_retweet_network('D:/Work/Databases/Tweets/EleccionesEEUU/Tweets')
+
+net_manager.write_network_as_gpickle_file(DiG, 'us_elections_2018_retweets')
+
 
--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # ODIN (Outlier Detection on Information Networks)
 
-This project is experimental in nature and not very stable at the moment. The anomaly detection algorithms work!
+This project provides a set of algorithms and tools to perform anomaly
+detection on complex networks. The project is experimental in nature and
+stability is not one of its main goals at the moment.
+
+WARNING: The project does not work with NetworkX 2.x and its being
+migrated right now. The code compatible with NetworkX 1.x can be found
+in the folder odin_nx1.
 
 Dependencies:
 -scikit-feature
